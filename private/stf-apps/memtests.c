12fa514c35f96926491f4e367e28795eee0bffb7
Arthur Jones <arthur@lbl.gov>
2002-11-27 06:48:44 -0600

    add sfi code...

---
+++ private/stf-apps/memtests.c
@@ -0,0 +1,376 @@
+/*
+ * Very simple (yet, for some reason, very effective) memory tester.
+ * Originally by Simon Kirby <sim@stormix.com> <sim@neato.org>
+ * Version 2 by Charles Cazabon <memtest@discworld.dyndns.org>
+ *
+ * This file contains the functions for the actual tests, called from the
+ * main routine in memtest.c.  See other comments in that file.
+ *
+ */
+#include <stdio.h>
+
+#define RAND32 0x16383245
+#define OK 0
+#define ERROR 1
+
+typedef unsigned int ui32;
+
+#include "sfi.h"
+
+/* Data. */
+int silent_test = 0;
+
+/* Function definitions. */
+static int test_verify_success (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  
+  for (i = 0; i < count; i++, p1++, p2++) {
+    if (*p1 != *p2) {
+      printf("FAILURE: 0x%08x != 0x%08x at offset 0x%08x.\r\n",
+	       *p1, *p2, i);
+      return ERROR;
+    }
+  }
+  return OK;
+}
+
+static int test_random_value(ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  
+  silent_test = 0;		
+  for (i = 0; i < count; i++) {
+    *p1++ = *p2++ = RAND32;
+  }
+  return test_verify_success (bp1, bp2, count);
+}
+
+static int test_xor_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  ui32 q = RAND32;
+  
+  silent_test = 0;		
+  for (i = 0; i < count; i++)
+    {
+      *p1++ ^= q;
+      *p2++ ^= q;
+    }
+  return (test_verify_success (bp1, bp2, count));
+}
+
+static int test_sub_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  ui32 q = RAND32;
+  
+  silent_test = 0;		
+  for (i = 0; i < count; i++)
+    {
+      *p1++ -= q;
+      *p2++ -= q;
+    }
+  return (test_verify_success (bp1, bp2, count));
+}
+
+static int test_mul_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  ui32 q = RAND32;
+  
+  silent_test = 0;		
+  for (i = 0; i < count; i++)
+    {
+      *p1++ *= q;
+      *p2++ *= q;
+    }
+  return (test_verify_success (bp1, bp2, count));
+}
+
+static int test_div_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  ui32 q = RAND32;
+  
+  silent_test = 0;		
+  for (i = 0; i < count; i++) {
+    if (!q)
+      q++;
+    *p1++ /= q;
+    *p2++ /= q;
+  }
+  return (test_verify_success (bp1, bp2, count));
+}
+
+
+static int test_or_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  ui32 q = RAND32;
+  
+  silent_test = 0;		
+  for (i = 0; i < count; i++)
+    {
+      *p1++ |= q;
+      *p2++ |= q;
+    }
+  return (test_verify_success (bp1, bp2, count));
+}
+
+
+static int test_and_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  ui32 q = RAND32;
+  
+  silent_test = 0;		
+  for (i = 0; i < count; i++)
+    {
+      *p1++ &= q;
+      *p2++ &= q;
+    }
+  return (test_verify_success (bp1, bp2, count));
+}
+
+
+static int test_seqinc_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i;
+  ui32 q = RAND32;
+  
+  silent_test = 0;		
+  for (i = 0; i < count; i++)
+    {
+      *p1++ = *p2++ = (i + q);
+    }
+  return (test_verify_success (bp1, bp2, count));
+}
+
+static int test_solidbits_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 q, i, j;
+  
+  silent_test = 1;
+  for (j = 0; j < 64; j++)
+    {
+      q = (j % 2) == 0 ? 0xFFFFFFFF : 0x00000000;
+      p1 = (volatile ui32 *) bp1;
+      p2 = (volatile ui32 *) bp2;
+      for (i = 0; i < count; i++)
+	{
+	  *p1++ = *p2++ = (i % 2) == 0 ? q : ~q;
+	}
+      
+      if (test_verify_success (bp1, bp2, count) == ERROR)
+	{
+	  return (ERROR);
+	}
+    }
+  return  OK;
+}
+
+
+static int test_checkerboard_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 q, i, j;
+  
+  silent_test = 1;		
+  for (j = 0; j < 64; j++)
+    {
+      q = (j % 2) == 0 ? 0x55555555 : 0xAAAAAAAA;
+      p1 = (volatile ui32 *) bp1;
+      p2 = (volatile ui32 *) bp2;
+      for (i = 0; i < count; i++)
+	{
+	  *p1++ = *p2++ = (i % 2) == 0 ? q : ~q;
+	}
+      
+      if (test_verify_success (bp1, bp2, count) == ERROR)
+	{
+	  return (ERROR);
+	}
+    }
+  return (OK);
+}
+
+static int test_blockseq_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i, j;
+  
+  silent_test = 1;		
+  for (j = 0; j < 256; j++) {
+    p1 = (volatile ui32 *) bp1;
+    p2 = (volatile ui32 *) bp2;
+    for (i = 0; i < count; i++)
+      {
+	*p1++ = *p2++ = (int) (j);
+      }
+    
+    if (test_verify_success (bp1, bp2, count) == ERROR)
+      {
+	return (ERROR);
+      }
+  }
+  return (OK);
+}
+
+
+static int test_walkbits_comparison (ui32 *bp1, ui32 *bp2, ui32 count, int m) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i, j;
+  
+  silent_test = 1;		
+  for (j = 0; j < 64; j++)
+    {
+      p1 = (volatile ui32 *) bp1;
+      p2 = (volatile ui32 *) bp2;
+      for (i = 0; i < count; i++)
+	{
+	  if (j < 32)	/* Walk it up. */
+	    {
+	      *p1++ = *p2++ = (m == 0) ? 0x00000001 << j :
+		0xFFFFFFFF ^ (0x00000001 << j);
+	    }
+	  else		/* Walk it back down. */
+	    {
+	      *p1++ = *p2++ = (m == 0)
+		? 0x00000001 << (64 - j - 1)
+		: 0xFFFFFFFF ^ (0x00000001 << (64 - j - 1));
+	    }				
+	}
+      
+      if (test_verify_success (bp1, bp2, count) == ERROR)
+	{
+	  return (ERROR);
+	}
+    }
+  return (OK);
+}
+
+
+static int test_bitspread_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i, j;
+  
+  silent_test = 1;		
+  for (j = 0; j < 64; j++)
+    {
+      p1 = (volatile ui32 *) bp1;
+      p2 = (volatile ui32 *) bp2;
+      for (i = 0; i < count; i++)
+	{
+	  if (j < 32)	/* Walk it up. */
+	    {
+	      *p1++ = *p2++ = (i % 2 == 0)
+		? (0x00000001 << j) | (0x00000001 << (j + 2))
+		: 0xFFFFFFFF ^ ((0x00000001 << j)
+				| (0x00000001 << (j + 2)));
+	    }
+	  else		/* Walk it back down. */
+	    {
+	      *p1++ = *p2++ = (i % 2 == 0)
+		? (0x00000001 << (63 - j)) | (0x00000001 << (65 - j))
+		: 0xFFFFFFFF ^ (0x00000001 << (63 - j) 
+				| (0x00000001 << (65 - j)));
+	    }				
+	}
+      
+      if (test_verify_success (bp1, bp2, count) == ERROR)
+	{
+	  return (ERROR);
+	}
+    }
+  return (OK);
+}
+
+static int test_bitflip_comparison (ui32 *bp1, ui32 *bp2, ui32 count) {
+  volatile ui32 *p1 = (volatile ui32 *) bp1;
+  volatile ui32 *p2 = (volatile ui32 *) bp2;
+  ui32 i, j, k;
+  ui32 q;
+  
+  silent_test = 1;		
+  for (k = 0; k < 32; k++)
+    {
+      q = 0x00000001 << k;
+      
+      for (j = 0; j < 8; j++)
+	{
+	  q = ~q;
+	  p1 = (volatile ui32 *) bp1;
+	  p2 = (volatile ui32 *) bp2;
+	  for (i = 0; i < count; i++) {
+	      *p1++ = *p2++ = (i % 2) == 0 ? q : ~q;
+	  }
+	  
+	  if (test_verify_success (bp1, bp2, count) == ERROR) {
+	    return (ERROR);
+	  }
+	}
+    }
+  return (OK);
+}
+
+static int test_stuck_address (ui32 *bp1, ui32 *unused, ui32 count) {
+  volatile ui32 *p1;
+  /* second argument is not used; just gives it a compatible signature. */
+  ui32 i, j;
+  int err = OK;
+
+  for (j = 0; j < 16; j++)
+    {
+      p1 = (volatile ui32 *) bp1;
+      for (i = 0; i < count; i++, p1++)
+	{
+	  *p1 = ((j + i) % 2) == 0 ? (ui32) p1 : ~((ui32) p1);
+	}
+
+      p1 = (volatile ui32 *) bp1;
+      for (i = 0; i < count; i++, p1++)
+	{
+	  if (*p1 != (((j + i) % 2) == 0 ? (ui32) p1 : ~((ui32) p1)))
+	    {
+	      printf("FAILURE: offset: "
+		     " %d (rep %d). [%08x should be: %08x]\r\n", i, j,
+		     *p1,
+		     (((j + i) % 2) == 0 ? (ui32) p1 : ~((ui32) p1)));
+	      err = ERROR;
+	    }
+	}
+    }
+  return err;
+}
+
+void initMemTests(void) {
+  addConstantBucket("memtest-silent", (int) &silent_test);
+  addCFunc3Bucket("memtest-random-value", (CFunc3) test_random_value);
+  addCFunc3Bucket("memtest-xor-comparison", (CFunc3) test_xor_comparison);
+  addCFunc3Bucket("memtest-sub-comparison", (CFunc3) test_sub_comparison);
+  addCFunc3Bucket("memtest-mul-comparison", (CFunc3) test_mul_comparison);
+  addCFunc3Bucket("memtest-div-comparison", (CFunc3) test_div_comparison);
+  addCFunc3Bucket("memtest-or-comparison", (CFunc3) test_or_comparison);
+  addCFunc3Bucket("memtest-and-comparison", (CFunc3) test_and_comparison);
+  addCFunc3Bucket("memtest-seqinc-comparison", (CFunc3) test_seqinc_comparison);
+  addCFunc3Bucket("memtest-solidbits-comparison", (CFunc3) test_solidbits_comparison);
+  addCFunc3Bucket("memtest-checkerboard-comparison", (CFunc3) test_checkerboard_comparison);
+  addCFunc3Bucket("memtest-blockseq-comparison", (CFunc3) test_blockseq_comparison);
+  addCFunc3Bucket("memtest-walkbits-comparison", (CFunc3) test_walkbits_comparison);
+  addCFunc3Bucket("memtest-bitspread-comparison", (CFunc3) test_bitspread_comparison);
+  addCFunc3Bucket("memtest-bitflip-comparison", (CFunc3) test_bitflip_comparison);
+  addCFunc3Bucket("memtest-stuck-address", (CFunc3) test_stuck_address);
+}
