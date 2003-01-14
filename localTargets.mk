5b801e7e42e8a80a251053acf65cee10c4e841be
Simon Patton <SJPatton@lbl.gov>
2003-01-14 15:29:29 -0600

    Removed double reference to platfrom in auto-generation

--- localTargets.mk
+++ localTargets.mk
@@ -1,6 +1,14 @@
+$(BUILD_DIR)/$(PUB_DIR_NAME)/%.h : $(PUB_DIR_NAME)/$(PLATFORM)/%.xml
+	@test -d $(@D) || mkdir -p $(@D)
+	$(XSLT) -in $< -out $@ -xsl $(PRIVATE_DIR_NAME)/stf-apps/stfDefn2Header.xsl
+
 $(BUILD_DIR)/$(PUB_DIR_NAME)/%.h : $(PUB_DIR_NAME)/%.xml
 	@test -d $(@D) || mkdir -p $(@D)
 	$(XSLT) -in $< -out $@ -xsl $(PRIVATE_DIR_NAME)/stf-apps/stfDefn2Header.xsl
+
+$(BUILD_DIR)/$(PVT_DIR_NAME)/%.h : $(PVT_DIR_NAME)/$(PLATFORM)%.xml
+	@test -d $(@D) || mkdir -p $(@D)
+	$(XSLT) -in $< -out $@ -xsl $(<D)/stfDefn2Header.xsl
 
 $(BUILD_DIR)/$(PVT_DIR_NAME)/%.h : $(PVT_DIR_NAME)/%.xml
 	@test -d $(@D) || mkdir -p $(@D)
