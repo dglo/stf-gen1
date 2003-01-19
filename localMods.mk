d0cb53c75bf6fc79e6d55a40b6d5e007dd58a33a
Simon Patton <SJPatton@lbl.gov>
2003-01-19 08:35:57 -0600

    Updated to handle generated .c files

--- localMods.mk
+++ localMods.mk
@@ -4,6 +4,9 @@ XML_HDRS_SEARCH := $(patsubst %,%/*.xml, $(PUB_DIRS) $(PVT_DIRS))
 XML_HDRS := $(wildcard $(XML_HDRS_SEARCH))
 BUILT_HDRS := $(patsubst %.xml, $(BUILD_DIR)/%.$(C_INC_SUFFIX), $(XML_HDRS))
 
-INCS := $(INCS) $(BUILT_HDRS)
+INCS := $(INCS)
+
+BUILT_FILES := $(BUILD_DIR)/$(PVT_DIR_NAME)/stf-apps/stfDictionary.c $(BUILT_HDRS)
 
 vpath %.xml $(PUB_DIR_NAME)/$(PLATFORM) $(PUB_DIR_NAME) $(PVT_DIR_NAME)/$(PLATFORM) $(PVT_DIR_NAME)
+vpath %.xsd $(PUB_DIR_NAME)/$(PLATFORM) $(PUB_DIR_NAME) $(PVT_DIR_NAME)/$(PLATFORM) $(PVT_DIR_NAME)
--- localTargets.mk
+++ localTargets.mk
@@ -5,3 +5,7 @@ $(BUILD_DIR)/$(PUB_DIR_NAME)/%.h : %.xml
 $(BUILD_DIR)/$(PVT_DIR_NAME)/%.h : %.xml
 	@test -d $(@D) || mkdir -p $(@D)
 	$(XSLT) -in $< -out $@ -xsl $(<D)/stfDefn2Header.xsl
+
+$(BUILD_DIR)/$(PVT_DIR_NAME)/%Dictionary.c : %.xsd
+	@test -d $(@D) || mkdir -p $(@D)
+	$(XSLT) -in $< -out $@ -xsl $(<D)/xsd2Dictionary.xsl
