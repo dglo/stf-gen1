516b3d8994d92b5536bd87fd698017e343442b96
Simon Patton <SJPatton@lbl.gov>
2003-04-26 19:15:18 -0500

    Set up correct Variables for epxa10 to work

--- project.mk
+++ project.mk
@@ -1,5 +1,12 @@
-C_BIN_NAMES:=gendir genqry menu stfserv
-USES_PROJECTS:=hal
-USES_TOOLS:=libxml expat z m
+ifeq ("epxa10","$(strip $(PLATFORM))")
+  C_BIN_NAMES := 
+  C_EXCLUDE_NAMES := menu stfserv gendir genqry mk-name stftcp stf stflient
+  USES_TOOLS := expat z m
+else
+  C_BIN_NAMES := gendir genqry menu stfserv
+  C_EXCLUDE_NAMES := stf stflient
+  USES_TOOLS := libxml expat z m
+endif
+USES_PROJECTS := hal
 
 include ../resources/standard.mk
