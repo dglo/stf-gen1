5da08e66daa7c89f18b0557745697bcb2c4d4f76
Simon Patton <SJPatton@lbl.gov>
2003-04-26 19:29:36 -0500

    Correct lists for Linux

--- project.mk
+++ project.mk
@@ -1,10 +1,10 @@
 ifeq ("epxa10","$(strip $(PLATFORM))")
   C_BIN_NAMES := 
-  C_EXCLUDE_NAMES := menu stfserv gendir genqry mk-name stftcp stf stflient
+  C_EXCLUDE_NAMES := stfserv gendir genqry mk-name stftcp menu stfclient
   USES_TOOLS := expat z m
 else
   C_BIN_NAMES := gendir genqry menu stfserv
-  C_EXCLUDE_NAMES := stf stflient
+  C_EXCLUDE_NAMES := stfclient
   USES_TOOLS := libxml expat z m
 endif
 USES_PROJECTS := hal
