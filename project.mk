206a16fd9798a469fc313b984f9f104c82b8d2ea
Simon Patton <SJPatton@lbl.gov>
2003-05-15 10:13:24 -0500

    Introduced recursive calling to allow clean to work better

--- project.mk
+++ project.mk
@@ -9,4 +9,18 @@ else
   USES_PROJECTS := $(filter-out dom-loader, $(USES_PROJECTS))
 endif
 
-include ../resources/standard.mk
+# The normal include of standard.mk is not used here so that the 'clean' target
+# does not create the BUILT_FILES needed to get the dependencies correct.
+#include ../resources/standard.mk
+
+project.mk: ;
+
+Makefile: ;
+
+force: ;
+
+%: force
+	@$(MAKE) -f ../resources/standard.mk PROJECT="$(PROJECT)" PLATFORM="$(PLATFORM)" ICESOFT_HOST="$(ICESOFT_HOST)" C_BIN_NAMES="$(C_BIN_NAMES)" C_EXCLUDE_NAMES="$(C_EXCLUDE_NAMES)" USES_PROJECTS="$(USES_PROJECTS)" USES_TOOLS="$(USES_TOOLS)" $@
+
+clean:
+	@$(MAKE) -f ../resources/standard.mk PROJECT="$(PROJECT)" PLATFORM="$(PLATFORM)" ICESOFT_HOST="$(ICESOFT_HOST)" C_BIN_NAMES="$(C_BIN_NAMES)" C_EXCLUDE_NAMES="$(C_EXCLUDE_NAMES)" USES_PROJECTS="$(USES_PROJECTS)" USES_TOOLS="$(USES_TOOLS)" BUILT_FILES="" BUILD_DEPENDS="" $@
