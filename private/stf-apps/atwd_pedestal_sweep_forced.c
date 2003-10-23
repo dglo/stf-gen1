79389ffffe3c44734568c66a9f5b6909afbcecd7
Arthur Jones <arthur@lbl.gov>
2003-10-23 09:07:28 -0500

    fix pass/no pass bug

--- private/stf-apps/atwd_pedestal_sweep_forced.c
+++ private/stf-apps/atwd_pedestal_sweep_forced.c
@@ -135,7 +135,7 @@ BOOLEAN atwd_pedestal_sweep_forcedEntry(STF_DESCRIPTOR *d,
       *full_range_counts_per_volt > 400 &&
       *full_range_counts_per_volt < 700 &&
       *linearity_pedestal_percent > 95 &&
-      *linearity_pedestal_percent > 105; 
+      *linearity_pedestal_percent < 105; 
 }
 
 
