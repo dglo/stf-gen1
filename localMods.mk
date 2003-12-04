2c38db9e6e37c0cddae6830297b151053fb41499
Simon Patton <SJPatton@lbl.gov>
2003-12-04 05:58:25 -0600

    Fixed to pick up only test Defn XML

--- localMods.mk
+++ localMods.mk
@@ -1,10 +1,12 @@
+XML_DEFN_DIRS := private/stf-apps
+
 # Add auto-generated include files to list of includes
 XML_HDR_SEARCH := $(patsubst %,%/*.xml, $(PUB_DIRS) $(PVT_DIRS))
-XML_HDRS_SRCS := $(wildcard $(XML_HDR_SEARCH))
+XML_HDRS_SRCS := $(foreach XML_SRC_DIR, ${XML_DEFN_DIRS}, $(filter $(XML_SRC_DIR)/%,$(wildcard $(XML_HDR_SEARCH))))
 XML_HDRS := $(patsubst %.xml,$(BUILD_DIR)/%.$(C_INC_SUFFIX), $(XML_HDRS_SRCS))
 XML_HDR_TARGETS :=$(subst $(PLATFORM_PUB_ROOT)/$(PLATFORM),$(PUB_ROOT), $(subst $(PVT_ROOT)/$(PLATFORM),$(PVT_ROOT), $(XML_HDRS)))
 
-DEFAULT_XML_TARGETS := $(patsubst %.xml,$(COMMON_RSRC_DIR)/%Default.xml, $(subst private/stf-apps/,, $(XML_HDRS_SRCS)))
+DEFAULT_XML_TARGETS := $(foreach XML_SRC_DIR, ${XML_DEFN_DIRS}, $(patsubst %.xml,$(COMMON_RSRC_DIR)/%Default.xml, $(subst $(XML_SRC_DIR)/,,$(filter $(XML_SRC_DIR)/%,$(XML_HDRS_SRCS)))))
 
 BUILT_FILES += $(XML_HDR_TARGETS) $(BUILD_DIR)/$(PVT_DIR_NAME)/stf-apps/stfDictionary.c $(DEFAULT_XML_TARGETS)
 
