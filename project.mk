f94ec516a9f46943589f970fc711f62a76648d9a
Simon Patton <SJPatton@lbl.gov>
2004-03-05 06:42:35 -0600

    Added math library

--- project.mk
+++ project.mk
@@ -4,7 +4,7 @@ USES_PROJECTS := iceboot dom-loader hal
 USES_TOOLS := libxml expat z m
 
 ifeq ("epxa10","$(strip $(PLATFORM))")
-  USES_TOOLS := $(filter-out libxml m, $(USES_TOOLS))
+  USES_TOOLS := $(filter-out libxml, $(USES_TOOLS))
 else
   USES_PROJECTS := $(filter-out dom-loader, $(USES_PROJECTS))
 endif
