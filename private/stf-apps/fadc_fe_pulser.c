d2afbe9931675e75db18c2d7b2b799ef73df6c28
Arthur Jones <arthur@lbl.gov>
2004-03-25 12:21:45 -0600

    seems to work now, convert to new scanSPE routine and clean up a bit...

--- private/stf-apps/fadc_fe_pulser.c
+++ private/stf-apps/fadc_fe_pulser.c
@@ -1,15 +1,17 @@
 /* fadc_fe_pulser.c, skeleton file created by gendir
    code added by Eric G. Henson eghenson@lbl.gov 
 */
-#include "stdio.h"
-#include "stdlib.h"
-#include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 #include "stf/stf.h"
 #include "stf-apps/fadc_fe_pulser.h"
 
 #include "hal/DOM_MB_hal.h"
 #include "hal/DOM_MB_fpga.h"
+#include "stf-apps/atwdUtils.h"
 
 BOOLEAN fadc_fe_pulserInit(STF_DESCRIPTOR *d) {
    return TRUE;
@@ -20,35 +22,21 @@ BOOLEAN fadc_fe_pulserEntry(STF_DESCRIPTOR *d,
                     unsigned atwd_pedestal_dac,
                     unsigned long pulser_amplitude_uvolt,
                     unsigned long loop_count,
-
                     unsigned *triggerable_spe_dac,
                     unsigned *fadc_baseline_mean,
                     unsigned *fadc_fe_pulser_amplitude,
                     unsigned *fadc_fe_pulser_width,
                     unsigned *fadc_fe_pulser_position,
                     unsigned *fadc_fe_pulser_waveform) {
-
-  DOM_HAL_FPGA_PULSER_RATES rate;
-  unsigned  spe_dac_nominal,scan_start,scan_stop;
-  unsigned read_zero,read_nonzero,read_fail;
   unsigned baseline,baseline_sum,baseline_mean,waveform_mean;
   unsigned temp,pos,amp;
   unsigned lp1,lp2,actual_loops;
-  unsigned scan_value,spe_rate;
   unsigned half_max,start_width,end_width;
-  unsigned discrim_in_mv = 0;
   int time_out,time_out_error=0;
   short *waveform = (short *) calloc(512, sizeof(short));
-  int *waveform_sum = (int *) calloc(512, sizeof(int));                                                                             
+  int *waveform_sum = (int *) calloc(512, sizeof(int));
   int *waveform_avg = (int *) calloc(512, sizeof(int));
- 
-  /*  short *waveform_sum = (short *) calloc(512, sizeof(short));
-      short *waveform_avg = (short *) calloc(512, sizeof(short));*/
- 
-  /*  unsigned waveform[512];
-  unsigned waveform_sum[512];
-  unsigned waveform_avg[512];*/
-
+  
   /*  A.Pretest checks:*/
   /*  1.The two input DAC settings are programmed.*/
   halWriteDAC(DOM_HAL_DAC_FAST_ADC_REF,fadc_reference_dac);
@@ -61,90 +49,41 @@ BOOLEAN fadc_fe_pulserEntry(STF_DESCRIPTOR *d,
   halPowerDownBase();
 
   /*  3.Set the Front-End pulser amplitude DAC to zero.*/
-  halWriteDAC(DOM_HAL_DAC_INTERNAL_PULSER,0);
-  halUSleep(500000);
-
   /*  4.Turn ON  the front end pulser.*/
    /* pretest 4) turn on fe pulser */
-  lookupPulserRate(78e3, &rate, NULL);
-  hal_FPGA_TEST_set_pulser_rate(rate);
-  hal_FPGA_TEST_enable_pulser();
-
-  /*  5.Find if there is a reasonable value for the SPE discriminator level such that will not trigger on noise.
-      (Because part values vary from board to board and the noise band is narrow
-      the discriminator value must be scanned). The nominal value is obtained from:
-      SPE DAC_NOMINAL = (Discrim_in_milivolts * 9.6 * (2240+249)/249 + ATWD_PEDESTAL_DAC * 5,000 /4096) *1024 / 5,000. */
-  spe_dac_nominal = (discrim_in_mv * 95.961445783 + atwd_pedestal_dac * 1.220703125) * 0.2048;
-
+  /*  5.Find if there is a reasonable value for the SPE discriminator level 
+      such that will not trigger on noise.
+      (Because part values vary from board to board and the noise band is 
+      narrow
+      the discriminator value must be scanned). The nominal value is obtained 
+      from:
+      SPE DAC_NOMINAL = (Discrim_in_milivolts * 9.6 * (2240+249)/249 + ATWD_PEDESTAL_DAC * 5,000 /4096) *1024 / 5,000. 
+  */
   /*  6.Scan SPE_DAC downwards from SPE DAC_NOMINAL+5% to SPE DAC_NOMINAL-5% in 1-count steps (integer values),
       at each value set the SPE_DAC and obtain the SPE rate meter value
       (this counts discriminator crossings in 100 msec, so this step will be relatively time-consuming).
       Continue the scan until a Non-zero value is found.*/
-  read_fail = 0;
-  read_zero = 0;
-  read_nonzero = 0;
-  scan_start = (unsigned)floor((double)spe_dac_nominal * 1.05);
-  scan_stop  = (unsigned)floor((double)spe_dac_nominal * 0.95); /*0.95);*/
-  for(scan_value=scan_start;scan_value>scan_stop;scan_value--)
-  {
-       halWriteDAC(DOM_HAL_DAC_SINGLE_SPE_THRESH,scan_value);
-       halUSleep(110000); /*wait 110ms */
-       spe_rate = hal_FPGA_TEST_get_spe_rate();
-  
-       if(read_zero && read_nonzero)
-	 break;
-       if(read_zero && !read_nonzero)
-	 *triggerable_spe_dac = scan_value;
-       if(!read_zero && read_nonzero)
-       {
-           read_fail = 1;
-           break;
-       }
-       if(!spe_rate)
-	 read_zero = 1;
-       if(spe_rate)
-	 read_nonzero = 1;
-  }
-
 /*  7.Save the first SPE_DAC setting for which there is a Non-Zero value of the SPE rate meter.                        
     If there is NO transition from 0 rate to non-zero rate the test FAILS and QUITS and                                 
     returns in TRIGGERABLE_SPE_DAC the largest SPE_DAC tried if there were no zeros found and  
     smallest SPE_DAC tried if they were all zeros.*/
-
-  if(!read_zero)       /*never read zero*/
-  {
-    *triggerable_spe_dac = scan_start;
-    read_fail = 1;
-  }
-  if(read_zero && !read_nonzero)     /*read all zeros*/
-  {
-    *triggerable_spe_dac = scan_stop;
-    read_fail = 1;
-  }
-  if(read_fail)
-  {
-    /*printf("read failed\n\r");*/
+  /*  8.Set the TRIGGERABLE_SPE_DAC output variable to
+      2 + the first SPE_DAC setting that gave a Non-Zero value of the SPE rate.*/
+  if (scanSPE(atwd_pedestal_dac, triggerable_spe_dac)) {
+     /* printf("read failed\r\n"); */
     *fadc_baseline_mean = 0;
     *fadc_fe_pulser_amplitude = 0;
     *fadc_fe_pulser_width = 0;
     *fadc_fe_pulser_position = 0;
-    for(lp1=0;lp1<512;lp1++)
-    {
-      fadc_fe_pulser_waveform[lp1] = 0;
-    }
-
-       free(waveform); 
-       free(waveform_sum); 
-       free(waveform_avg); 
-       return FALSE;
+    memset(fadc_fe_pulser_waveform, 0, 512*sizeof(unsigned));
+    free(waveform); 
+    free(waveform_sum); 
+    free(waveform_avg); 
+    return FALSE;
   }
 
-  /*  8.Set the TRIGGERABLE_SPE_DAC output variable to
-      2 + the first SPE_DAC setting that gave a Non-Zero value of the SPE rate.*/
-  *triggerable_spe_dac+=2;
-
   /*  9.Set the SPE_DAC to the TRIGGERABLE_SPE_DAC value.*/
-  halWriteDAC(DOM_HAL_DAC_SINGLE_SPE_THRESH,*triggerable_spe_dac); 
+  halWriteDAC(DOM_HAL_DAC_SINGLE_SPE_THRESH, *triggerable_spe_dac); 
   halUSleep(100000);
        
   /*  B.Test algorithm: */
@@ -166,6 +105,7 @@ BOOLEAN fadc_fe_pulserEntry(STF_DESCRIPTOR *d,
     waveform_mean /=512;
     baseline = waveform_mean;
 
+
   /*  3.Keep a sum of all baselines.*/
     baseline_sum += baseline;
   
@@ -185,6 +125,13 @@ BOOLEAN fadc_fe_pulserEntry(STF_DESCRIPTOR *d,
   temp = (pulser_amplitude_uvolt*25)/5000;
   halWriteDAC(DOM_HAL_DAC_INTERNAL_PULSER,temp);
   halUSleep(100000);
+
+  {  DOM_HAL_FPGA_PULSER_RATES rate;
+     lookupPulserRate(78e3, &rate, NULL);
+     hal_FPGA_TEST_set_pulser_rate(rate);
+     hal_FPGA_TEST_enable_pulser();
+     halUSleep(10000);
+  }
 
   /*   8.Take LOOP_COUNT waveforms for the FADC with the SPE- trigger,
        keeping a sum-waveform to obtain the average waveform
@@ -298,3 +245,14 @@ BOOLEAN fadc_fe_pulserEntry(STF_DESCRIPTOR *d,
 
  return TRUE; 
 }
+
+
+
+
+
+
+
+
+
+
+
