0915716e978c8042a993ff9c72f243c34c594748
CVS-to-SVN conversion program <cvs2svn@icecube.wisc.edu>
2004-04-16 10:11:41 -0500

    This commit was manufactured by cvs2svn to create tag 'V01-01-04'.

--- private/stf-apps/disc_scan.c
+++ private/stf-apps/disc_scan.c
@@ -5,9 +5,10 @@
 
 #include "stf-apps/atwdUtils.h"
 
-BOOLEAN disc_scanInit(STF_DESCRIPTOR *d) {
-   return FALSE;
-}
+#include "hal/DOM_MB_hal.h"
+#include "hal/DOM_MB_fpga.h"
+
+BOOLEAN disc_scanInit(STF_DESCRIPTOR *d) { return TRUE; }
 
 BOOLEAN disc_scanEntry(STF_DESCRIPTOR *d,
 		       unsigned disc_scan_window_uvolts,
@@ -24,7 +25,17 @@ BOOLEAN disc_scanEntry(STF_DESCRIPTOR *d,
 		       unsigned *disc_scan_noise_uvolt,
 		       unsigned *disc_sum_waveform) {
    int zero_dac = speUVoltToDAC(0, atwd_pedestal_dac);
-   int window_dac /* = () */;
+   int window_dac = speUVoltToDAC(disc_scan_window_uvolts, 0);
+   int i;
+   DOM_HAL_FPGA_PULSER_RATES rate;
+
+   /* pretest 4) turn on fe pulser */
+   lookupPulserRate(78e3, &rate, NULL);
+   hal_FPGA_TEST_set_pulser_rate(rate);
+   hal_FPGA_TEST_enable_pulser();
+
+   prescanATWD(HAL_FPGA_TEST_TRIGGER_ATWD0);
+
    return FALSE;
 }
 
