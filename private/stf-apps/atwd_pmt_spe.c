fcfd74f95985b3f372dd64c09895ea6e3af2a638
Arthur Jones <arthur@lbl.gov>
2004-05-05 09:31:04 -0500

    better pass/fail conditions, some formatting from george

--- private/stf-apps/atwd_pmt_spe.c
+++ private/stf-apps/atwd_pmt_spe.c
@@ -45,7 +45,7 @@ BOOLEAN atwd_pmt_speEntry(STF_DESCRIPTOR *d,
    int *sum_waveform = (int *) calloc(128, sizeof(int));
    int trigger_mask = (atwd_chip_a_or_b) ? 
       HAL_FPGA_TEST_TRIGGER_ATWD0 : HAL_FPGA_TEST_TRIGGER_ATWD1;
-   int spe_dac_nominal, pmt_dac, pulser_or_not=0;
+   int spe_dac_nominal, pmt_dac, use_pulser=0;
 
    /* pretest 1) all five atwd dac settings are programmed... */
    halWriteDAC(ch, atwd_sampling_speed_dac);
@@ -64,7 +64,7 @@ BOOLEAN atwd_pmt_speEntry(STF_DESCRIPTOR *d,
     halWriteActiveBaseDAC(pmt_hv_low_volt*2); /* for pmt, input_dac=input_volt*2 */
    
    /* pretest 4) turn on pmt */
-   if (scanSPE(atwd_pedestal_dac, triggerable_spe_dac, pulser_or_not)) {
+   if (scanSPE(atwd_pedestal_dac, triggerable_spe_dac, use_pulser)) {
       /* no triggerable value found... */
       free(buffer);
       return FALSE;
@@ -96,7 +96,7 @@ BOOLEAN atwd_pmt_speEntry(STF_DESCRIPTOR *d,
    pmt_dac = pmt_hv_high_volt*2;
    halEnableBaseHV();
    halWriteActiveBaseDAC(pmt_dac);
-   *real_hv_output = (halReadBaseDAC()/2); 
+   *real_hv_output = halReadBaseADC(); 
 
    /* wait for dacs, et al... */
    halUSleep(1000*100);
@@ -146,10 +146,10 @@ BOOLEAN atwd_pmt_speEntry(STF_DESCRIPTOR *d,
        int half_max, hmxIdx = 127, hmnIdx = 0;
        
        for (i=1; i<128; i++) {
-	  if (maxValue < atwd_waveform_pmt_spe[i]) {
-	     maxIdx = i;
-	     maxValue = atwd_waveform_pmt_spe[i];
-	  }
+	 if (maxValue < atwd_waveform_pmt_spe[i]) {
+	   maxIdx = i;
+	   maxValue = atwd_waveform_pmt_spe[i];
+	 }
        }
 
        /* 10) */
@@ -160,16 +160,16 @@ BOOLEAN atwd_pmt_speEntry(STF_DESCRIPTOR *d,
 	  (*atwd_waveform_amplitude)/2 + *atwd_baseline_waveform;
 
        for (i=maxIdx; i<cnt; i++) {
-	  if (atwd_waveform_pmt_spe[i]<half_max) {
-	     hmxIdx = i;
-	     break;
-	  }
+	 if (atwd_waveform_pmt_spe[i]<half_max) {
+	   hmxIdx = i;
+	   break;
+	 }
        }
        for (i=maxIdx; i>=0; i--) {
-	  if (atwd_waveform_pmt_spe[i]<half_max) {
-	     hmnIdx = i;
-	     break;
-	  }
+	 if (atwd_waveform_pmt_spe[i]<half_max) {
+	   hmnIdx = i;
+	   break;
+	 }
        }
 
        /* 14 */
@@ -187,15 +187,7 @@ BOOLEAN atwd_pmt_speEntry(STF_DESCRIPTOR *d,
       *atwd_waveform_position > 2 &&
       *atwd_waveform_position < 10 &&
       *atwd_waveform_width > 2 &&
-      *atwd_waveform_width < 6;
+      *atwd_waveform_width < 6 &&
+      *real_hv_output > 0.95*pmt_hv_high_volt &&
+      *real_hv_output < 1.05*pmt_hv_high_volt;
 }
-
-
-
-
-
-
-
-
-
-
