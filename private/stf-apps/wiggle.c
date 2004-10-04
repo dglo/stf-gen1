f046ed45e3719594adda8b10384173576581f29d
Arthur Jones <arthur@lbl.gov>
2004-10-04 08:46:19 -0500

    wiggle test

---
+++ private/stf-apps/wiggle.c
@@ -0,0 +1,37 @@
+/* wiggle.c -- wiggle comm dac in 
+ */
+#include "hal/DOM_MB_fpga.h"
+#include "hal/DOM_MB_pld.h"
+
+#ifndef WIGGLE_SHIFT
+#define WIGGLE_SHIFT 0
+#endif
+
+/* wiggle for 1500ms break for 100ms -- wiggle rate is approx
+ * 1MHz we're done after about 20s...
+ */
+int main(void) {
+   int j;
+   
+   for (j=0; j<10; j++) {
+      int rand = 0;
+      int i;
+      
+      for (i=0; i<750 * 1000; i++) {
+         char v;
+         rand = rand*69069 + 1;
+         halUSleep(1);
+         hal_FPGA_TEST_comm_bit_bang_dac( (rand&0xff)>>WIGGLE_SHIFT );
+      }
+      halUSleep(1);
+      hal_FPGA_TEST_comm_bit_bang_dac(128);
+      halUSleep(100*1000);
+   }
+
+   halBoardReboot();
+   
+   return 0;
+}
+
+
+
